2e12832f11092cce9410bc149faa4dd99f1a7cbc
Corysia Taware <corysia.taware@gmail.com>
2024-11-22 20:15:52 -0800

    Updated README

--- Assets/Scripts/DetectVR.cs
+++ Assets/Scripts/DetectVR.cs
@@ -112,7 +112,7 @@ public class DetectVR : MonoBehaviour
     private void EnableDesktopCamera()
     {
         // Switch from the VR camera to the desktop camera.
-        Debug.Log($"Enabling Desktop Camera");
+        Debug.Log("<color=green>Enabling Desktop Camera</color>");
         GameManager.instance.inVR = false;
 
         // Disable the XR Origin.
@@ -125,31 +125,63 @@ public class DetectVR : MonoBehaviour
     /// <summary>
     /// Switches from the VR camera to the desktop camera.
     /// </summary>
+    ContextMenu("Switch to Desktop Camera")
     private void SwitchToDesktopCamera()
     {
         // Set the desktop camera's scale to the same as the XR origin's scale.
         desktopCamera.transform.localScale = xrOrigin.transform.localScale;
 
+        // Set the desktop camera's position to the same as the XR origin's position.
+        desktopCamera.transform.position = xrOrigin.transform.position;
+
+        // Set the desktop camera's rotation to the same as the XR origin's rotation.
+        desktopCamera.transform.rotation = xrOrigin.transform.rotation;
+
         // Enable the desktop camera.
         EnableDesktopCamera();
     }
 
+    /// <summary>
+    /// Switches from the desktop camera to the VR camera.
+    /// </summary>
+    ContextMenu("Switch to VR Camera")
+    private void SwitchToVRCamera()
+    {
+        EnableVRCamera();
+    }
+
+    /// <summary>
+    /// Switches from the desktop camera to the VR camera.
+    /// </summary>
+    /// <returns>true if the switch was successful, false otherwise.</returns>
     private bool EnableVRCamera()
     {
-        Debug.Log("Enabling VR Camera");
+        Debug.Log("<color=green>Enabling VR Camera</color>");
+        
+        if (GameManager.instance.inVR) return true;
+
+        // Set the XR origin's scale to the same as the desktop camera's scale.
+        xrOrigin.transform.localScale = desktopCamera.transform.localScale;
+        
+        // Set the XR origin's position to the same as the desktop camera's position.
+        xrOrigin.transform.position = desktopCamera.transform.position;
+        
+        // Set the XR origin's rotation to the same as the desktop camera's rotation.
+        xrOrigin.transform.rotation = desktopCamera.transform.rotation;
+        
         GameManager.instance.inVR = true;
         GameManager.instance.vrCapable = true;
         var xrSettings = XRGeneralSettings.Instance;
         if (xrSettings == null)
         {
-            Debug.Log($"XRGeneralSettings is null.");
+            Debug.Log("XRGeneralSettings is null.");
             return false;
         }
 
         var xrManager = xrSettings.Manager;
         if (xrManager == null)
         {
-            Debug.Log($"XRManagerSettings is null.");
+            Debug.Log("XRManagerSettings is null.");
             return false;
         }
 
@@ -157,8 +189,38 @@ public class DetectVR : MonoBehaviour
         if (xrLoader == null) xrManager.InitializeLoader();
         xrManager.InitializeLoader();
 
-        xrOrigin.gameObject.SetActive(true); // Enable the XR Origin.
-        desktopCamera.gameObject.SetActive(false); // Disable the Desktop Camera.
+        // Enable the XR origin.
+        xrOrigin.gameObject.SetActive(true);
+        
+        // Disable the desktop camera.
+        desktopCamera.gameObject.SetActive(false);
         return true;
+    }
+
+    /// <summary>
+    /// Switch between the desktop camera and the VR camera when the TAB key is pressed.
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameManager.instance.inVR)
+            {
+                // Switch from the VR camera to the desktop camera.
+                SwitchToDesktopCamera();
+            }
+            else
+            {
+                // Switch from the desktop camera to the VR camera.
+                SwitchToVRCamera();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 This is a [Unity](https://unity.com/) example project.  It demonstrates how to create a game that can run in either VR or Desktop mode.
 
-**NOTE:** This project is currently illustrating a bug in the XR system where if the XR Rig is disbled, but "Start in VR" is checked in the XR Plugin Management section, the game loop locks up and Update() calls are executed once and once only.
+**NOTE:** This project is currently illustrating a bug in the XR system where if the XR Rig is disbled, but "Start in VR" is checked in the XR Plugin Management section, the game loop locks up and Update() calls are executed once and once only.  `Assets/Scene/SampleScene` is configured to show the issue I'm running in to.
 
